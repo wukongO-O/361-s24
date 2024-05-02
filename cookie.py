1ef7c7bead45c44fa891aa472b3e26f1d9c349f1
wukongO-O <l.shenglan@gmail.com>
2024-05-02 11:51:54 -0400

    Outlined app design

--- cookie.py
+++ cookie.py
@@ -2,6 +2,70 @@ import urllib.request
 import ssl
 from bs4 import BeautifulSoup
 
+def fetch_github_db(url):
+    """
+    Scrape the textarea content from the github link and return the data in a list
+    """
+    pass
+
+def get_random_content(contents):
+    """
+    Return a random element from the given list 
+    """
+    pass
+
+def generate_cookie_id():
+    """
+    Generate unique id for a cookie
+    """
+    pass
+
+def show_features():
+    """
+    Print existing and new features
+    """
+    pass
+
+def show_help():
+    """
+    Display available commands and tutorials
+    """
+    pass
+
+def exit_app():
+    """
+    Terminate the app
+    """
+    pass
+
+def save_cookies(cookies):
+    """
+    Save the chosen or all cookies 
+    """
+    pass
+
+
+def main():
+    # greet users
+
+    # fetch quotes and words 
+
+    # track cookie ids and contents  
+
+    # ask for user name and email
+
+
+    # if users want to see features 
+
+    # if users want to see command list
+
+    # if users want to save cookies
+
+    # if users choose to exit
+
+    pass
+
+
 # Fetch all quotes
 url_quote = "https://github.com/aussieW/skill-confucius-say/blob/master/dialog/en-us/sayings.dialog"
 
